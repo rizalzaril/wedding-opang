4707d5e894bb71b8d952e781e3d1cf8a8cca1e87
Rizal <zaril.ziral2020@gmail.com>
2024-11-08 19:01:13 +0700

    push

--- gsapScript.js
+++ gsapScript.js
@@ -43,31 +43,33 @@
 gsap.registerPlugin(ScrollTrigger);
 
 // Select all elements with the class .photo
-document.querySelectorAll(".photo").forEach((photo) => {
-  gsap.to(photo, {
-    scrollTrigger: {
-      trigger: photo,
-      scrub: false,
-      markers: true,
-      toggleActions: "restart pause resume none",
-    },
-    y: 10,
-    rotationY: 180, // Flip backward
-    duration: 1,
-    onComplete: () => {
-      // After flipping backward, flip forward
-      gsap.to(photo, {
-        rotationY: 0, // Flip forward
-        duration: 1,
-        scrollTrigger: {
-          trigger: photo,
-          scrub: false,
-          markers: false,
-        },
-      });
-    },
+document
+  .querySelectorAll(".transition-flip-360")
+  .forEach((transitionflip360) => {
+    gsap.to(transitionflip360, {
+      scrollTrigger: {
+        trigger: transitionflip360,
+        scrub: false,
+        markers: true,
+        toggleActions: "restart pause resume none",
+      },
+      y: 5,
+      rotationY: 180, // Flip backward
+      duration: 1,
+      onComplete: () => {
+        // After flipping backward, flip forward
+        gsap.to(transitionflip360, {
+          rotationY: 0, // Flip forward
+          duration: 1,
+          scrollTrigger: {
+            trigger: transitionflip360,
+            scrub: false,
+            markers: false,
+          },
+        });
+      },
+    });
   });
-});
 
 gsap.registerPlugin(ScrollTrigger);
 
