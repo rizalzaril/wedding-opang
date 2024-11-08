0c8c065684ae5d60a272d4c3bf917f2b46e43df8
Rizal <zaril.ziral2020@gmail.com>
2024-11-08 16:20:15 +0700

    upload

--- gsapScript.js
+++ gsapScript.js
@@ -38,64 +38,63 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".h1-scroll", {
-  scrollTrigger: {
-    trigger: ".h1-scroll",
-    // start: "top center",
-    // markers: true,
-    toggleActions: "restart pause resume none",
-  },
-  x: 0, // Move to center
-  opacity: 1, // Make it visible
-  duration: 1,
+// gsap.to(".h1-scroll", {
+//   scrollTrigger: {
+//     trigger: ".h1-scroll",
+//     // start: "top center",
+//     // markers: true,
+//     toggleActions: "restart pause resume none",
+//   },
+//   x: 0, // Move to center
+//   opacity: 1, // Make it visible
+//   duration: 1,
+// });
+
+// JavaScript with GSAP
+gsap.utils.toArray(".transition-scale-scroll").forEach((element, index) => {
+  gsap.to(element, {
+    scrollTrigger: {
+      trigger: element,
+      start: "top 85%", // Starts animation slightly later for a softer entrance
+      toggleActions: "restart none restart none",
+      markers: false, // Hide markers for a cleaner look
+    },
+    scale: 1, // Full scale at the end
+    opacity: 1, // Full visibility at the end
+    duration: 1.2, // Longer duration for smoother animation
+    ease: "power2.out", // Softer easing for smoothness
+    display: "block",
+    // delay: index * 0.15, // Less delay for more continuity
+  });
 });
 
-gsap.to(".h1-scroll-story", {
-  scrollTrigger: {
-    trigger: ".h1-scroll-story",
-    // start: "top center",
-    // markers: true,
-    toggleActions: "restart pause resume none",
-  },
-  x: 0, // Move to center
-  opacity: 1, // Make it visible
-  duration: 1,
-});
-
-gsap.to(".h1-scroll-gallery", {
-  scrollTrigger: {
-    trigger: ".h1-scroll-gallery",
-    // start: "top center",
-    // markers: true,
-    toggleActions: "restart pause resume none",
-  },
-  x: 0, // Move to center
-  opacity: 1, // Make it visible
-  duration: 1,
-});
-
-// Animate each section as it comes into view
+// Select all sections with the class .love-story
 let sections = gsap.utils.toArray(".love-story");
 
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".scroll-container",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    end: () => "+=" + document.querySelector(".scroll-container").offsetWidth,
-  },
-});
+// Set up the horizontal scroll animation
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1), // Move each section horizontally by 100%
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".scroll-container", // Container that holds the sections
+//     pin: true, // Pin the container during scroll
+//     scrub: 1, // Smooth, synced scrolling
+//     snap: {
+//       snapTo: 1 / (sections.length - 1), // Snap to each section
+//       duration: 0.2, // Duration of the snapping animation
+//       ease: "power1.inOut", // Smooth snap effect
+//     },
+//     end: () => "+=" + document.querySelector(".scroll-container").scrollWidth, // End after the total scrollable width
+//   },
+// });
 
 gsap.to(".col-home-scroll-up", {
   scrollTrigger: {
     trigger: ".col-home-scroll-up",
-    // start: "top center bottom",
+    start: "top center bottom",
     //scrub: true,
     // markers: true,
-    toggleActions: "restart pause resume none",
+    toggleActions: "restart none restart none",
   },
   x: 0,
   opacity: 1,
@@ -105,10 +104,10 @@ gsap.to(".col-home-scroll-up", {
 gsap.to(".col-home-scroll-down", {
   scrollTrigger: {
     trigger: ".col-home-scroll-down",
-    // start: "top center bottom",
+    start: "top center bottom",
     //scrub: true,
     // markers: true,
-    toggleActions: "restart pause resume none",
+    toggleActions: "restart none restart none",
   },
   x: 0,
   opacity: 1,
